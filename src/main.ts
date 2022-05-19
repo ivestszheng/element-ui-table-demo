c0ad60de4e5693e5ad38f1e93455637b6e02d680
无声 <ivestszheng@qq.com>
2022-05-19 15:25:41 +0800

    feat: 实现合并内容

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
 import { createPinia, PiniaVuePlugin } from 'pinia';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
 Vue.use(VueCompositionAPI);
-
+Vue.use(ElementUI);
 Vue.use(PiniaVuePlugin);
 const pinia = createPinia();
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,117 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <el-table :data="tableData" class="my-table" :header-cell-class-name="genHeaderCellClassName">
+    <!-- <el-table-column label="配送信息" width="780"> -->
+    <el-table-column label="配送信息" width="780" fixed="left">
+      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+      <el-table-column label="地址" width="660">
+        <el-table-column prop="province" label="省份" width="120"> </el-table-column>
+        <el-table-column prop="city" label="市区" width="120"> </el-table-column>
+        <el-table-column prop="address" label="地址" width="300"> </el-table-column>
+        <el-table-column prop="zip" label="邮编" width="120"> </el-table-column>
+      </el-table-column>
+    </el-table-column>
+    <el-table-column prop="date" label="日期" width="150"> </el-table-column>
+    <el-table-column label="空" width="150"> </el-table-column>
+  </el-table>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default Vue.extend({
+<script>
+export default {
   name: 'HomeView',
-  components: {
-    HelloWorld,
+  data() {
+    this.tableData = [
+      {
+        date: '2016-05-03',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333,
+      },
+      {
+        date: '2016-05-02',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333,
+      },
+      {
+        date: '2016-05-04',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333,
+      },
+      {
+        date: '2016-05-01',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333,
+      },
+      {
+        date: '2016-05-08',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333,
+      },
+      {
+        date: '2016-05-06',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333,
+      },
+      {
+        date: '2016-05-07',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333,
+      },
+    ];
+    return {};
   },
-});
+  methods: {
+    genHeaderCellClassName({ row, column, rowIndex, columnIndex }) {
+      // 省份列
+      if (rowIndex === 2 && columnIndex === 0) {
+        this.$nextTick(() => {
+          const cells = document.getElementsByClassName(column.id);
+          // 代表此时有 fixed 列
+          if (cells.length > this.tableData.length) {
+            const bottomHeader = cells[0];
+            const topHeader = cells[this.tableData.length + 1];
+
+            bottomHeader.setAttribute('colspan', 2);
+            topHeader.setAttribute('colspan', 2);
+          } else {
+            const targetHeader = cells[0];
+
+            targetHeader.setAttribute('colspan', 2);
+          }
+        });
+      }
+      // 给被合并的表头增加标识
+      if (rowIndex === 2 && columnIndex === 1) return 'cell-hide';
+      return null;
+    },
+  },
+};
 </script>
+<style lang="less" scoped>
+.my-table {
+  width: 800px;
+  margin: 0 auto;
+}
+.el-table /deep/ .cell-hide {
+  display: none;
+}
+</style>
